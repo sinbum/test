4cb0fcea9fea3202c4e3f266d23b1a74597f0cd0
Sinbum <79790395+sinbum@users.noreply.github.com>
2022-06-15 22:40:14 +0900

    이것은 파이썬에서 os.system 으로 보내는 커밋

---
+++ GitMain.py
@@ -0,0 +1,16 @@
+import requests
+import json
+import os
+
+
+# os.system("git --version")
+# os.system("node --version")
+
+# print("cd .\\test\\")
+
+os.system("cd .\\test\\")
+os.system("pwd")
+os.system("git add --all")
+os.system("git commit -m \"이것은 파이썬에서 os.system 으로 보내는 커밋\"  ")
+os.system("git push")
+
